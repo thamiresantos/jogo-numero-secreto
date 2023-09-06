3c41a39df6e84cb9d6169ebcef06169791d41c0a
Thamires Santos <106447459+thamiresantos@users.noreply.github.com>
2023-09-05 22:43:50 -0300

    jogo numero secreto

---
+++ app/reconhecimentoDeVoz.js
@@ -0,0 +1,27 @@
+const elementoChute = document.getElementById('chute')
+
+window.SpeechRecognition = window.SpeechRecognition || 
+webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.lang = 'pt-br' //idioma
+recognition.start()
+
+recognition.addEventListener('result', onSpeak) 
+
+//função para aparecer o resultado do reconhecimento de voz
+function onSpeak(e) {
+    chute = e.results[0][0].transcript
+    exibeChuteNaTela(chute)
+    verificaSeOChutePossuiUmValorValido (chute)
+}
+
+function exibeChuteNaTela(chute){
+    elementoChute.innerHTML = `
+    <div>Você disse</div>
+    <span class="box">${chute}</span>
+    `
+}
+
+//evento. Quando a função acabar ele ligar mais uma vez o reconhecimento de voz
+recognition.addEventListener('end', () => recognition.start())
